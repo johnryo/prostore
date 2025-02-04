d8a840bfc72ccfc459b9a7f278db041bde8b2dcf
johnryo <jpogge55@gmail.com>
2025-02-03 16:50:05 -0800

    Get products for admin page; display products; delete products; create and update actions; create product page and form; form fields and slugify; create product form submission; uploadthing configuration; add image uploads; product cleanup; isFeatured and banner; product update form

--- app/(root)/place-order/page.tsx
+++ app/(root)/place-order/page.tsx
@@ -78,7 +78,6 @@ const PlaceOrderPage = async () => {
           <Card>
             <CardContent className='p-4 gap-4'>
               <h2 className='text-xl pb-4 font-semibold'>Order Items</h2>
-              <p>{user.paymentMethod}</p>
               <Table>
                 <TableHeader>
                   <TableRow>
